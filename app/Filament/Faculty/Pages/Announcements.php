52ee416a30fc05ad5b61050c08d64162f6ab8d34
kenneth <kennethdeleon7@gmail.com>
2023-10-17 13:22:16 +0800

    displaying announcements

--- app/Filament/Faculty/Pages/Announcements.php
+++ app/Filament/Faculty/Pages/Announcements.php
@@ -2,11 +2,45 @@
 
 namespace App\Filament\Faculty\Pages;
 
+use App\Domain\Announcement\Models\Announcement;
 use Filament\Pages\Page;
+use DateTimeZone;
+use Illuminate\Support\Carbon;
 
 class Announcements extends Page
 {
+    public mixed $announcementList;
+
     protected static ?string $navigationIcon = 'heroicon-o-megaphone';
 
     protected static string $view = 'filament.pages.faculty.announcements';
+
+    public function mount(): void
+    {
+        $this->fetchData();
+    }
+
+    public function fetchData(): void
+    {
+        $this->announcementList = $this->getAnnouncements();
+    }
+
+    public function getAnnouncements()
+    {
+        $result = Announcement::orderBy('created_at', 'desc')->get();
+
+        return $result;
+    }
+
+    public function dateFrom(Carbon $date): string
+    {
+        $carbonDate = Carbon::parse($date);
+
+        $userTimezone = 'Asia/Manila';
+        if (!empty($userTimezone) && in_array($userTimezone, DateTimeZone::listIdentifiers())) {
+            $carbonDate->setTimezone($userTimezone);
+        }
+
+        return $carbonDate->diffForHumans();
+    }
 }
--- resources/views/filament/pages/faculty/announcements.blade.php
+++ resources/views/filament/pages/faculty/announcements.blade.php
@@ -1,2 +1,56 @@
 <x-filament-panels::page>
+
+    @if (count($this->announcementList) == 0)
+        <div x-data="table" class="fi-ta" data-has-alpine-state="true" x-ignore="">
+            <div
+                class="fi-ta-ctn divide-y divide-gray-200 overflow-hidden rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:divide-white/10 dark:bg-gray-900 dark:ring-white/10">
+                <div
+                    class="fi-ta-content relative divide-y divide-gray-200 overflow-x-auto dark:divide-white/10 dark:border-t-white/10 !border-t-0">
+
+                    <div class="fi-ta-empty-state px-6 py-12">
+                        <div class="fi-ta-empty-state-content mx-auto grid max-w-lg justify-items-center text-center">
+                            <div
+                                class="fi-ta-empty-state-icon-ctn mb-4 rounded-full bg-gray-100 p-3 dark:bg-gray-500/20">
+                                <svg class="fi-ta-empty-state-icon h-6 w-6 text-gray-500 dark:text-gray-400"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12">
+                                    </path>
+                                </svg>
+                            </div>
+
+                            <h4
+                                class="fi-ta-empty-state-heading text-base font-semibold leading-6 text-gray-950 dark:text-white">
+                                No announcements
+                            </h4>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    @foreach ($this->announcementList as $announcement)
+        <x-filament::section>
+            <div class="flex items-center gap-x-3">
+
+                <div class="flex-1">
+                    <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
+                        {{ $announcement->title }}
+                    </h2>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">
+                        {{ $this->dateFrom($announcement->created_at) }}
+                    </p>
+
+                    <p class="text-sm text-gray-500 dark:text-gray-400 mt-6">
+                        {{ $announcement->content }}
+                    </p>
+                </div>
+
+            </div>
+        </x-filament::section>
+    @endforeach
+
+
 </x-filament-panels::page>
